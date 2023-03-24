195b3ba40126c3177c1b0d0cd60f1684bbe519e8
GUNTURU BAJIKUMAR <116821554+bajikumar9010@users.noreply.github.com>
2023-03-24 23:43:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import phonenumbers
+from phonenumbers import geocoder
+
+# Parse phone number
+phone_number = phonenumbers.parse("+91 give your phnumber", "IND")
+
+# Get location information
+location = geocoder.description_for_number(phone_number, "en")
+print(location)
+from phonenumbers import carrier
+service_number=phonenumbers.parse("+919010237748","RO")
+print(carrier.name_for_number(service_number,"en"))
+
+
