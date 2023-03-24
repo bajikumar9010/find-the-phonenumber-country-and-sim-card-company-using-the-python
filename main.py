e722b9ea2c47d23149a83a7fdaeefc7380c90853
GUNTURU BAJIKUMAR <116821554+bajikumar9010@users.noreply.github.com>
2023-03-24 23:44:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ phone_number = phonenumbers.parse("+91 give your phnumber", "IND")
 location = geocoder.description_for_number(phone_number, "en")
 print(location)
 from phonenumbers import carrier
-service_number=phonenumbers.parse("+919010237748","RO")
+service_number=phonenumbers.parse("+91give your number","RO")
 print(carrier.name_for_number(service_number,"en"))
 
 
